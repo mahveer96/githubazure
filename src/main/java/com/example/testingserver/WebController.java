ab27a61d76caa2686a094e4903c51c71a41b6eae
md96 <mahesh.tella@gmail.com>
2025-07-26 19:53:15 +0530

    Added MySQL DB configuration and entity

--- src/main/java/com/example/testingserver/WebController.java
+++ src/main/java/com/example/testingserver/WebController.java
@@ -1,6 +1,13 @@
 package com.example.testingserver;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.testingserver.entity.Station;
+import com.example.testingserver.service.StationService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,12 +15,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("/station")
 public class WebController {
+
+    @Autowired
+    private StationService stationService;
 
 	@GetMapping("/")
     public String home() {
         return "Spring Boot App Deployed Successfully!";
     }
 	
+
+    
+    @GetMapping
+    public List<Station> getAllStations() {
+        return stationService.Getallstations();
+    }
 	
 }
