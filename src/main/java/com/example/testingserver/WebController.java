0b0b81b059038dc8b6543c132bd124977507442e
md96 <mahesh.tella@gmail.com>
2025-07-26 21:44:33 +0530

    Add: your commit message here

--- src/main/java/com/example/testingserver/WebController.java
+++ src/main/java/com/example/testingserver/WebController.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +28,10 @@ public class WebController {
         return "Spring Boot App Deployed Successfully!";
     }
 	
+     @PostMapping
+    public Station createStation(@RequestBody Station station) {
+        return stationService.createStation(station);
+    }
 
     
     @GetMapping
